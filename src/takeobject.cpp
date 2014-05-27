7f0cdffecf1cd03ab0268a4b73a7d5a2f7254863
Noah Levy <nlevy@AVIRIS-CAL.jpl.nasa.gov>
2014-05-27 12:19:11 -0700

    implementing EDT code inside thread, was using C++11 threads but may switch to boost if I decide to use a boost ringbuffer

--- src/takeobject.cpp
+++ src/takeobject.cpp
@@ -6,23 +6,36 @@
  */
 
 #include "takeobject.hpp"
-
-take_object::take_object(int channel_num)
+#include <iostream>
+take_object::take_object(int channel_num, int number_of_buffers)
 {
 	this->channel = channel_num;
+	this->numbufs = number_of_buffers;
 }
 take_object::~take_object()
 {
-	// TODO Auto-generated destructor stub
+	pdv_close(pdv_p);
+	delete this->pdv_p;
 }
 
 
 void take_object::start()
 {
-	PdvDev * pdv_p = NULL;
-	pdv_p = pdv_open_channel(EDT_INTERFACE,0,this->channel);
+	//The internet says that I need to pass a pointer to the threadable function, and a reference to the calling instance of take_object (this)
+	pdv_thread = std::thread(&take_object::pdv_init, this);
+}
+void take_object::pdv_init()
+{
+	this->pdv_p = NULL;
+	this->pdv_p = pdv_open_channel(EDT_INTERFACE,0,this->channel);
 	if(pdv_p == NULL)
 	{
-		//throw exception
+		std::cout << "Oh gawd, couldn't open channel" << std::endl;
+		//TODO: throw exception
+		return;
 	}
+	//TODO: Sizing stuff
+	int size = pdv_get_dmasize(pdv_p); //Not using this at the moment
+	pdv_multibuf(pdv_p,this->numbufs);
+
 }
--- src/takeobject.hpp
+++ src/takeobject.hpp
@@ -13,12 +13,18 @@
 #include <mutex>
 #include "edtinc.h"
 class take_object {
-	std::thread t;
+	PdvDev * pdv_p;
+	std::thread pdv_thread;
 	unsigned int channel;
+	unsigned int numbufs;
+	unsigned int frame_memory_size;
 public:
-	take_object(int channel_num = 2);
+	take_object(int channel_num = 2, int number_of_buffers = 4, int fmsize = 1000);
 	virtual ~take_object();
 	void start();
+private:
+	void pdv_init();
+
 };
 
 #endif /* TAKEOBJECT_HPP_ */
