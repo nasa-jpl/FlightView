30800cb13b043e6e5c3bbfe218222a5292f80857
Elliott H. Liggett <elliott.h.liggett@jpl.nasa.gov>
2023-05-31 11:41:26 -0700

    Minor change to saveServer.

--- saveserver.cpp
+++ saveserver.cpp
@@ -38,7 +38,6 @@ void saveServer::reconnectSignal()
         connect(clientConnection, SIGNAL(readyRead()), this, SLOT(readCommand()));
         signalConnected = true;
     }
-
 }
 
 void saveServer::disconnectSignal()
@@ -100,26 +99,25 @@ void saveServer::readCommand()
     {
         if (clientConnection->bytesAvailable() < (qint64) sizeof(quint16)) {
             //clientConnection->disconnectFromHost();
-            genStatusMessage("No further data available.");
+            //genStatusMessage("No further data available over tcp/ip.");
             readMutex.unlock();
             readData = false;
             return;
         }
         in >> blockSize;
-        qInfo() << "Block size: " << blockSize;
-
+        //qInfo() << "Block size: " << blockSize;
 
         if (clientConnection->bytesAvailable() < blockSize) {
             std::stringstream msg;
             msg << "Only " << clientConnection->bytesAvailable() << " bytes to read compared with block of " << blockSize << " bytes." << std::endl;
             genErrorMessage(QString::fromStdString(msg.str()));
-            clientConnection->disconnectFromHost();
+            //clientConnection->disconnectFromHost();
             readMutex.unlock();
             return;
         }
 
         in >> commandType;
-        genStatusMessage("Command received."); // DEBUG message, disable on release build
+        // genStatusMessage("Command received."); // DEBUG message, disable on release build
 
         switch (commandType) {
         case CMD_START_SAVING:
@@ -176,9 +174,11 @@ void saveServer::readCommand()
             out.setVersion(QDataStream::Qt_4_0);
             out << (uint16_t)0; // will be changed to the size of the message later.
             out << (uint16_t)CMD_STATUS_EXTENDED; // new addition
+            // NOTE: When the total number of frames to be saved is undefined (start/stop recording mode),
+            // the value returned is bogus and should not be used.
             out << (uint16_t)reference->to.save_framenum; // send the number of frames left to save, and
-            out << (uint16_t)reference->delta;            // send the frames per second
-            out << (uint16_t)reference->navgs;                       // number of averages, new code, bogus
+            out << (uint16_t)reference->delta;            // send the frames per second (as a uint)
+            out << (uint16_t)reference->navgs;            // number of averages, new code, bogus
             if(fname.isEmpty())
                 out << QString("");
             else
@@ -192,14 +192,12 @@ void saveServer::readCommand()
         case CMD_START_DARKSUB:
         {
             genStatusMessage("Client requested CMD_START_DARKSUB, starting dark collection.");
-            //reference->startCapturingDSFMask();
             emit startTakingDarks();
             break;
         }
         case CMD_STOP_DARKSUB:
         {
             genStatusMessage("Client requested CMD_STOP_DARKSUB, stopping dark collection.");
-            //reference->finishCapturingDSFMask();
             emit stopTakingDarks();
             break;
         }
@@ -209,16 +207,8 @@ void saveServer::readCommand()
             clientConnection->disconnectFromHost();
             break;
         }
-
-//        if(blockSize < 0)
-//        {
-//            readData = false;
-//        } else {
-//            readData = true;
-//        }
     }
     readMutex.unlock();
-
 }
 
 void saveServer::genErrorMessage(QString errorMessage)
